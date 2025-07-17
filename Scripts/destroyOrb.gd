20826225ed92b27816e0301fd4ca72c6ab0fae65
DylanFry <dylan-fry@live.nmit.ac.nz>
2025-07-17 13:44:41 +1200

    Added particle effect on hit

--- Scripts/destroyOrb.gd
+++ Scripts/destroyOrb.gd
@@ -16,7 +16,15 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("player"):
 		Speedcontrol.totalOrbs += 1
 		Speedcontrol.hitOrbs += 1
-		queue_free()
+		
+	# Burst and delete
+		$MeshInstance2D.visible = false
+		$Area2D.set_deferred("monitoring", false)
+		$GPUParticles2D.emitting = true
+
 	if area.is_in_group("Missed"):
 		Speedcontrol.totalOrbs += 1
 		queue_free()
+
+func _on_burst_finished():
+	queue_free()
