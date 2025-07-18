6390ffe99e1976c3aa0ef1e5c9be5980803e7fc1
swivet-effect <dy.fry203@gmail.com>
2025-07-18 15:47:24 +1200

    Added miss indication

--- Scripts/destroyOrb.gd
+++ Scripts/destroyOrb.gd
@@ -16,15 +16,19 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("player"):
 		Speedcontrol.totalOrbs += 1
 		Speedcontrol.hitOrbs += 1
-		
-	# Burst and delete
 		$MeshInstance2D.visible = false
 		$Area2D.set_deferred("monitoring", false)
 		$GPUParticles2D.emitting = true
 
 	if area.is_in_group("Missed"):
 		Speedcontrol.totalOrbs += 1
-		queue_free()
+		$MeshInstance2D.visible = false
+		$Area2D.set_deferred("monitoring", false)
+		$CanvasModulate.visible = true
+		$CanvasModulate/Timer.start()
 
 func _on_burst_finished():
+	queue_free()
+
+func _on_timer_timeout() -> void:
 	queue_free()
