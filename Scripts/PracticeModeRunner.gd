077cefd37b63f8b0c86d1853f086b4812f6a4b09
swivet-effect <dy.fry203@gmail.com>
2025-07-27 19:55:26 +1200

    Added Practice mode

---
+++ Scripts/PracticeModeRunner.gd
@@ -0,0 +1,13 @@
+extends Marker2D
+
+@onready var firer = $"Practice Firer"
+var rng = RandomNumberGenerator.new()
+
+func move():
+	rotation_degrees = rng.randf_range(0,360)
+	firer.fire()
+
+func _ready():
+	Global.speed = 100
+	Global.reset = false
+	move()
